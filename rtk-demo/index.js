5c08dff47c64f7f6e14598d33954553599e75b40
Preveen Raj <preveenraj.dev@gmail.com>
2024-09-30 17:48:21 +0530

    async thunk in redux toolkit

--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -1,19 +1,22 @@
 const store = require('./app/store');
 const cakeActions = require('./features/cake/cakeSlice').cakeActions;
 const iceCreamActions = require('./features/icecream/icecreamSlice').iceCreamActions;
+const fetchUsers = require('./features/user/userSlice').fetchUsers;
 
 
-console.log("Initial Sate", store.getState());
+// console.log("Initial Sate", store.getState());
 const unsubscribe = store.subscribe(() => console.log("Updated State", store.getState()));
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.restocked(5));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.restocked(5));
+
+store.dispatch(fetchUsers());
 
 // unsubscribe();
